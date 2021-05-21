7f201cd179a95308601e7239db3e44a86a7b448c
Bernd_Mac <20151553+bernd1995@users.noreply.github.com>
2021-05-21 09:39:01 +0200

    General structure

---
+++ src/plots.jl
@@ -0,0 +1,2 @@
+# This file should contain all recipes for plotting correlators, effective masses, volume plots, fits and channel-spectra (something else?)
+# Automatically creating titles depending on settings in theory.jl (use Plots.default() for this)
---
+++ src/theory.jl
@@ -0,0 +1,2 @@
+# This file is supposed to do handle all specifications for separate theories like for instance `su2higgs()` sets all structs and functions to default which are needed for this particular theory.
+# Not calling any theory should give warnings when ambigious functions are called
