43343ff0fbd1dc4560ac5019820419d032d58a3c
FabianZierler <fzierler@users.noreply.github.com>
2021-05-23 21:50:09 +0200

    Enable citations in docs using DocumenterCitations.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,4 +1,14 @@
 using Documenter
+using DocumenterCitations
 using LatSpec
 
-makedocs(sitename="LatSpec.jl")
+bib = CitationBibliography("LatSpec.bib")
+makedocs(
+    bib,
+    sitename="LatSpec.jl",
+    pages = [
+        "Home"       => "index.md",
+        "References" => "references.md"
+    ],
+    format = Documenter.HTML(prettyurls = get(ENV, "CI", nothing) == "true")
+)
