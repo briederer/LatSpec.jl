170c087caf360e614ecb8091d7987a078ae75fd2
Bernd_Mac <20151553+bernd1995@users.noreply.github.com>
2021-05-25 09:17:30 +0200

    Preparation for Docs CI

--- docs/make.jl
+++ docs/make.jl
@@ -3,12 +3,20 @@ using DocumenterCitations
 using LatSpec
 
 bib = CitationBibliography("LatSpec.bib")
+const PAGES = [
+               "Home"       => "index.md",
+               "References" => "references.md"
+              ]
+
 makedocs(
     bib,
     sitename="LatSpec.jl",
-    pages = [
-        "Home"       => "index.md",
-        "References" => "references.md"
-    ],
-    format = Documenter.HTML(prettyurls = get(ENV, "CI", nothing) == "true")
+    pages = PAGES,
+    format = Documenter.HTML(prettyurls = get(ENV, "CI", nothing) == "true"),
+    authors = "Bernd Riederer and Fabian Zierler"
+)
+
+deploydocs(
+    repo = "github.com/bernd1995/LatSpec.jl",
+    push_review = true
 )
